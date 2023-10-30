489ff0096b91f7fe429a346891caea7d748ddd0b
FahimmalAisyah <fahimmal.sasad@gmail.com>
2023-10-30 21:07:03 +0700

    first commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+
+import { cartContext } from "../store/shopping-cart-context";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const { items } = useContext(cartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -10,7 +16,7 @@ export default function Cart({ items, onUpdateItemQuantity }) {
       {items.length === 0 && <p>No items in cart!</p>}
       {items.length > 0 && (
         <ul id="cart-items">
-          {items.map((item) => {
+          {cartCtx.items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
---
+++ src/store/shopping-cart-context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const createContext = createContext({
+    items: [],
+    addit
+})
